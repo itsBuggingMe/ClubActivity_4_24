5ab1a756c8882d3abf48fd14daaf2d5b77d5f694
itsBuggingMe <lohj002@gmail.com>
2025-04-24 10:25:47 -0400

    Add project files.

---
+++ ClubActivity_4_24/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClubActivity_4_24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
+        string article = Web.RequestString(url);
+
+        Console.WriteLine(article);
+
+        // Look on here for more APIs
+        // https://github.com/public-apis/public-apis
+    }
+}
---
+++ ClubActivity_4_24/Web.cs
@@ -0,0 +1,29 @@
+﻿namespace ClubActivity_4_24;
+
+internal static class Web
+{
+    private static readonly HttpClient _shared = new();
+    public static string RequestString(string url)
+    {
+        try
+        {
+            return _shared.GetStringAsync(url).Result;
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+
+    public static Image RequestImage(string url)
+    {
+        try
+        {
+            return new Image(_shared.GetStreamAsync(url).Result);
+        }
+        catch
+        {
+            return new Image(Stream.Null);
+        }
+    }
+}
